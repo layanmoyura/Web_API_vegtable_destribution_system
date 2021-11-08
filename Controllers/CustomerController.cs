e87677a97423bed32f88b288504552ee038fbe70
kishanthan-s <kishanthansukumar@gmail.com>
2021-11-08 10:14:05 +0530

    added customer controller

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Online_platform_for_vegetables.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
